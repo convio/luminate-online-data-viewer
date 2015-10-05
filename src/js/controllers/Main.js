f511c8ac854778b8de6533605638c96989156125
Noah Cooper <noah.cooper@hotmail.com>
2015-10-05 11:55:47 -0400

    v0.0.18

--- src/js/controllers/Main.js
+++ src/js/controllers/Main.js
@@ -3,4 +3,45 @@ dataViewerControllers.controller('MainController', ['$scope', '$route', '$locati
   
   $scope.$route = $route;
   $scope.$location = $location;
+  
+  var checkForUpdates = function(showNoNewModal) {
+    $.ajax({
+      url: 'https://raw.githubusercontent.com/convio/luminate-online-data-viewer/master/package.json', 
+      dataType: 'json', 
+      error: function() {
+        /* TODO */
+      }, 
+      success: function(response) {
+        var activeVersion = $scope.manifest.version, 
+        currentVersion = response.version || activeVersion, 
+        currentVersionIsNew = false;
+        
+        if(activeVersion.split('.')[0] < currentVersion.split('.')[0]) {
+          currentVersionIsNew = true;
+        }
+        else if(activeVersion.split('.')[0] === currentVersion.split('.')[0] && 
+                activeVersion.split('.')[1] < currentVersion.split('.')[1]) {
+          currentVersionIsNew = true;
+        }
+        else if(activeVersion.split('.')[0] === currentVersion.split('.')[0] && 
+                activeVersion.split('.')[1] === currentVersion.split('.')[1] && 
+                activeVersion.split('.')[2] < currentVersion.split('.')[2]) {
+          currentVersionIsNew = true;
+        }
+        
+        if(!currentVersionIsNew && showNoNewModal) {
+          $('#no-new-version-modal').modal('show');
+        }
+        else if(currentVersionIsNew) {
+          $('#new-version-modal').modal('show');
+        }
+      }
+    });
+  };
+  
+  checkForUpdates(false);
+  
+  $scope.checkForUpdates = function() {
+    checkForUpdates(true);
+  };
 }]);
